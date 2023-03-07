74eb1dbad7c117366364da8622533b4f349442b7
Togrul85 <toghrulkhs@code.edu.az>
2023-03-07 14:06:44 +0400

    IdentityStart

---
+++ FrontToBack2/DAL/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+using FrontToBack2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrontToBack2.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public   DbSet<Slider> Sliders { get; set; }
+        public DbSet<SliderDetail> SliderDetails { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductImage> ProductImages { get; set; }
+
+      
+    }
+}
